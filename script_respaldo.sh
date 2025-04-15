dc40e33595b1f8e3f30b94be5c4c1a5de461b1d1
Stefany <jehm.medina@gmail.com>
2025-04-14 22:32:02 -0500

    Crear registro de cada copia con fecha y hora

--- script_respaldo.sh
+++ script_respaldo.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 fecha=$(date +%Y-%m-%d)
-if [ -f docs/informe.txt]; then
+if [ -f docs/informe.txt ]; then
 	cp docs/informe.txt respaldos/informe_$fecha.txt
 	echo "Respaldo creado en lab_semana5/respaldos/informe_$fecha.txt"
+	hora=$(date "+%Y-%m-%d %H:%M:%S")
+	echo "[${hora}] Respaldo creado: informe_${fecha}.txt" >> log_respaldo.txt
 else
 	echo "El archivo NO existe"
 fi
